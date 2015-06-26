11ebc9c7f1484e2edd04cf2565e276f8d50a8345
artog <adam.ingmansson@gmail.com>
2015-06-26 10:21:24 +0200

    Url patterns

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,9 +1,12 @@
 
 from django.conf.urls import url
-from .views import read, write
+from .views import read, write, new, join
 
 urlpatterns = [
     url(r'^$', read, name='story_index' ),
     url(r'^read$', read, name='story_read' ),
-    url(r'^write$', write, name='story_write' ),
+    url(r'^write?/$', write, name='story_write' ),
+    url(r'^write/new?/$', new, name='story_new' ),
+    url(r'^write/join?/$', join, name='story_join' ),
+    url(r'^write/(?P<story_id>[0-9]+)$', write, name='story_write' ),
 ]
